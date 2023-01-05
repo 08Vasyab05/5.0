eee1c7bf9468b216c839da299498a6b9f1142183
unknown <VasyaB2005@example.com>
2023-01-05 10:32:04 +0200

    first master commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double k (const double x, const double y);
+
+int main()
+{
+	double p, q;
+
+	cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+
+	double c = k(1 + p * q, q * q) + k * k(p, p * p) / 1 + k(p * q + q * q, p);
+
+	cout << "c = " << c << endl;
+
+	return 0;
+}
+
+double k(const double x, const double y)
+{
+	return x / (1 + sin(y) * sin(y)) + y / (1 + x * x);
+}
